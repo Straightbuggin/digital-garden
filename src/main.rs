ea381e314002935a07fb1ca1b8d660d286561a43
straightbuggin <alex.trei@gmail.com>
2023-07-30 10:29:42 -0700

    created the enum for the subcommand of write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+/// A CLI for the growing and curation of a digital garden
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(short = 'p', long, env)]
+    garden_path: Option<PathBuf>,
+
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file to your
+    /// garden
+    Write {
+        /// Optionally set a title for what youa re going to write about
+        #[clap(short, long)]
+        title: Option<String>,
+    },
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(args);
 }
